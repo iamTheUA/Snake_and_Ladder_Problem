f71aa3765be155ccc022b01f8e76dd8e50196949
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-27 18:12:12 +0530

    Rolling Dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,10 +1,19 @@
 package SnakeLadder;
 
-public class SnakeLadder {
+import java.util.Random;
 
+public class SnakeLadder {
+	
+	int DiceNum;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static int RollDice() {
+		Random ra = new Random();
+		int num = ra.nextInt(6) +1;
+		return num;
 	}
 }
 class Game{
