3f0ecd8c4e90cd79be82d46742f10099bcd53662
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-27 18:02:36 +0530

    First commit

---
+++ SnakeLadder.java
@@ -0,0 +1,15 @@
+package SnakeLadder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
+class Game{
+	Game(){
+		int Pos=0;
+		
+	}
+}
