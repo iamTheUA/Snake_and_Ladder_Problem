7548f42bd7eb7f90236d08307b1f2c6b91b0b339
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-27 18:21:45 +0530

    Adding Ladder..Sankes..and..No Moves...

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -15,6 +15,22 @@ public class SnakeLadder {
 		int num = ra.nextInt(6) +1;
 		return num;
 	}
+	
+	public static int Option() {
+		int op;
+		Random boy = new Random();
+		int temp= boy.nextInt(3);
+		if(temp==0) {
+			op=0;
+		}
+		else if(temp==1) {
+			op=1;
+		}
+		else {
+			op=-1;
+		}
+		return op;
+	}
 }
 class Game{
 	Game(){
