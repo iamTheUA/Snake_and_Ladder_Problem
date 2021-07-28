215dda71f757acfda3f2886801909c95c5f387ae
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-28 18:29:37 +0530

    Adding Two Player and Extra Play

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,52 +3,56 @@ package SnakeLadder;
 import java.util.Random;
 
 public class SnakeLadder {
-	
+	static int PositionOf1, PositionOf2, turn = 0;
+	static int EndPoint=100;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int turn=0;
-		Game P1 = new Game();
-		while(P1.pos<100) {
-			int DiceNum = RollDice();
-			int Option = Option();
-			int move=DiceNum*Option;
-			if (move+P1.pos>100) {
-				continue;
-			}
-			P1.pos+= move; 
-			if (P1.pos<0) {
-				P1.pos=0;
-			}
-			System.out.println("Position: "+P1.pos);
-			turn+=1;
+		while (PositionOf1 < EndPoint && PositionOf2 < EndPoint) {
+			PositionOf1 = Play(PositionOf1);
+			System.out.println("Position Player1: " + PositionOf1);
+			PositionOf2 = Play(PositionOf2);
+			System.out.println("Position Player2: " + PositionOf2);
+			System.out.println("");
+			turn += 1;
 		}
-		System.out.println("Number Of Turns: "+turn);
+		System.out.println("Number Of Turns : " + turn);
 	}
-	
-	public static int RollDice() {
-		Random ra = new Random();
-		int num = ra.nextInt(6) +1;
-		return num;
+
+	public static int Play(int pos) {
+		int DiceNum = RollDice();
+		int Option = Option();
+		int move = DiceNum * Option;
+		if (move + pos <= EndPoint) {
+			pos += move;
+		}
+		if (pos < 0) {
+			pos = 0;
+		}
+		if (Option == 1) {
+			return Play(pos);
+		}
+
+		return pos;
 	}
-	
+
 	public static int Option() {
 		int op;
-		Random boy = new Random();
-		int temp= boy.nextInt(3);
-		if(temp==0) {
-			op=0;
-		}
-		else if(temp==1) {
-			op=1;
-		}
-		else {
-			op=-1;
+		Random Random = new Random();
+		int temp = Random.nextInt(3);
+		if (temp == 0) {
+			op = 0;
+		} else if (temp == 1) {
+			op = 1;
+		} else {
+			op = -1;
 		}
 		return op;
 	}
-}
-class Game{
-	int pos=0;
-	
-	
+
+	public static int RollDice() {
+		Random Random = new Random();
+		int num = Random.nextInt(6) + 1;
+		return num;
+	}
+
 }
